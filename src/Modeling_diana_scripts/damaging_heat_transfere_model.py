c71da67ceacfdf7d788b4ba27c4204d8162f4b17
TheCodingBadger <147331534+TheCodingBadger@users.noreply.github.com>
2024-06-09 23:31:39 +0200

    Add files via upload

---
+++ src/Modeling_diana_scripts/damaging_heat_transfere_model.py
@@ -0,0 +1,8 @@
+# Dammmaging the bridge
+
+removeShape( [ "Cable_1NB" ] )
+remove( "GEOMETRYLOAD", [ "Post-tension load Cable_1NB" ] )
+
+
+removeShape( [ "Cable_2N" ] )
+remove( "GEOMETRYLOAD", [ "Post-tension load Cable_2N" ] )
